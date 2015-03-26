1f0f9e542649802d98f218e9364ab8ace6090028
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-25 20:07:13 -0400

    added some constants for the Ground and Fluttershy classes

--- app/constants.rb
+++ app/constants.rb
@@ -1,9 +1,19 @@
 module Constants
-  GAME_WIDTH  = 800
-  GAME_HEIGHT = 600
+  GAME_WIDTH          = 800
+  GAME_HEIGHT         = 600
   
-  GROUND_X_POS  = 0
-  GROUND_Y_POS  = 506
-  GROUND_WIDTH  = 800
-  GROUND_HEIGHT = 94
+  GROUND_X_POS        = 0
+  GROUND_Y_POS        = 506
+  GROUND_WIDTH        = 800
+  GROUND_HEIGHT       = 94
+  AUTO_SCROLL_X_SPEED = -250
+  AUTO_SCROLL_Y_SPEED = 0
+  
+  FLUTTERSHY_X_POS    = 270
+  FLUTTERSHY_Y_POS    = 420
+  GRAVITY             = 300
+  
+  WALKING_FRAMES      = [ "frame_000.png", "frame_001.png", "frame_002.png", "frame_003.png", "frame_004.png", "frame_005.png", "frame_006.png", "frame_007.png", "frame_008.png", "frame_009.png", "frame_010.png", "frame_011.png", "frame_012.png", "frame_013.png", "frame_014.png", "frame_015.png" ]
+  FRAME_RATE          = 20
+  LOOP                = true
 end
