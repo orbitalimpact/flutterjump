e784c0e032a177e8fb126b085a4920391fa6d21c
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-27 17:12:39 -0400

    added some constants

--- app/constants.rb
+++ app/constants.rb
@@ -11,9 +11,22 @@ module Constants
   
   FLUTTERSHY_X_POS    = 270
   FLUTTERSHY_Y_POS    = 420
+  
+  WALKING_WIDTH       = 102
+  WALKING_HEIGHT      = 86
+  FLYING_WIDTH        = 104
+  FLYING_HEIGHT       = 94
+  
   GRAVITY             = 300
+  JUMP_VELOCITY       = -300
+  
+  LEFT_VELOCITY       = -250
+  RIGHT_VELOCITY      = 300
+  
+  STOPPED             = 0
   
   WALKING_FRAMES      = [ "frame_000.png", "frame_001.png", "frame_002.png", "frame_003.png", "frame_004.png", "frame_005.png", "frame_006.png", "frame_007.png", "frame_008.png", "frame_009.png", "frame_010.png", "frame_011.png", "frame_012.png", "frame_013.png", "frame_014.png", "frame_015.png" ]
+  FLYING_FRAMES       = [ "frame_000.png", "frame_001.png", "frame_002.png", "frame_003.png", "frame_004.png", "frame_005.png", "frame_006.png", "frame_007.png", "frame_008.png", "frame_009.png", "frame_010.png", "frame_011.png", "frame_012.png", "frame_013.png", "frame_014.png", "frame_015.png" ]
   FRAME_RATE          = 20
   LOOP                = true
 end
