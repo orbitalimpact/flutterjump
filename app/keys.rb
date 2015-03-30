1f92503c83dbf3b0e5747909c468462b75fb750b
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-30 17:07:20 -0400

    add Keys class

---
+++ app/keys.rb
@@ -0,0 +1,27 @@
+class Keys
+  attr_reader :spacebar
+  attr_reader :left
+  attr_reader :right
+  attr_reader :a
+  attr_reader :d
+  
+  def initialize(game)
+    @game = game
+  end
+  
+  def preload
+    # nothing in here for this class
+  end
+  
+  def create
+    @spacebar = @game.input.keyboard.add_key(Phaser::Keyboard::SPACEBAR)
+    @left     = @game.input.keyboard.add_key(Phaser::Keyboard::LEFT)
+    @right    = @game.input.keyboard.add_key(Phaser::Keyboard::RIGHT)
+    @a        = @game.input.keyboard.add_key(Phaser::Keyboard::A)
+    @d        = @game.input.keyboard.add_key(Phaser::Keyboard::D)
+  end
+  
+  def update
+    # nothing in here for this class
+  end
+end
