e50a9d7ab8969996dfa641fe52d3ee9fb1c3b48b
George D. Plymale II <georgedp@orbitalimpact.com>
2015-04-01 23:09:01 -0400

    added constants for the obstacles and reformatted some code

--- app/constants.rb
+++ app/constants.rb
@@ -1,32 +1,39 @@
 module Constants
-  GAME_WIDTH          = 800
-  GAME_HEIGHT         = 600
+  GAME_WIDTH              = 800
+  GAME_HEIGHT             = 600
   
-  GROUND_X_POS        = 0
-  GROUND_Y_POS        = 506
-  GROUND_WIDTH        = 800
-  GROUND_HEIGHT       = 94
-  AUTO_SCROLL_X_SPEED = -250
-  AUTO_SCROLL_Y_SPEED = 0
+  GROUND_X_POS            = 0
+  GROUND_Y_POS            = 506
+  GROUND_WIDTH            = 800
+  GROUND_HEIGHT           = 94
+  AUTO_SCROLL_X_SPEED     = -250
+  AUTO_SCROLL_Y_SPEED     = 0
   
-  FLUTTERSHY_X_POS    = 270
-  FLUTTERSHY_Y_POS    = 420
+  FLUTTERSHY_X_POS        = 270
+  FLUTTERSHY_Y_POS        = 420
   
-  WALKING_WIDTH       = 102
-  WALKING_HEIGHT      = 86
-  FLYING_WIDTH        = 104
-  FLYING_HEIGHT       = 94
+  WALKING_WIDTH           = 102
+  WALKING_HEIGHT          = 86
+  FLYING_WIDTH            = 104
+  FLYING_HEIGHT           = 94
   
-  GRAVITY             = 300
-  JUMP_VELOCITY       = -300
+  GRAVITY                 = 300
+  JUMP_VELOCITY           = -300
   
-  LEFT_VELOCITY       = -250
-  RIGHT_VELOCITY      = 300
+  LEFT_VELOCITY           = -250
+  RIGHT_VELOCITY          = 300
   
-  STOPPED             = 0
+  STOPPED                 = 0
   
-  WALKING_FRAMES      = [ "frame_000.png", "frame_001.png", "frame_002.png", "frame_003.png", "frame_004.png", "frame_005.png", "frame_006.png", "frame_007.png", "frame_008.png", "frame_009.png", "frame_010.png", "frame_011.png", "frame_012.png", "frame_013.png", "frame_014.png", "frame_015.png" ]
-  FLYING_FRAMES       = [ "frame_000.png", "frame_001.png", "frame_002.png", "frame_003.png", "frame_004.png", "frame_005.png", "frame_006.png", "frame_007.png", "frame_008.png", "frame_009.png", "frame_010.png", "frame_011.png", "frame_012.png", "frame_013.png", "frame_014.png", "frame_015.png" ]
-  FRAME_RATE          = 20
-  LOOP                = true
+  WALKING_FRAMES          = [ "frame_000.png", "frame_001.png", "frame_002.png", "frame_003.png", "frame_004.png", "frame_005.png", "frame_006.png", "frame_007.png", "frame_008.png", "frame_009.png", "frame_010.png", "frame_011.png", "frame_012.png", "frame_013.png", "frame_014.png", "frame_015.png" ]
+  FLYING_FRAMES           = [ "frame_000.png", "frame_001.png", "frame_002.png", "frame_003.png", "frame_004.png", "frame_005.png", "frame_006.png", "frame_007.png", "frame_008.png", "frame_009.png", "frame_010.png", "frame_011.png", "frame_012.png", "frame_013.png", "frame_014.png", "frame_015.png" ]
+  FRAME_RATE              = 20
+  LOOP                    = true
+  
+  TOP_RANDOM_RANGE_MIN    = -150
+  TOP_RANDOM_RANGE_MAX    = -100
+  BOTTOM_RANDOM_RANGE_MIN = 400
+  BOTTOM_RANDOM_RANGE_MAX = 325
+  
+  TIME_INTERVAL           = 3
 end
