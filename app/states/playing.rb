01673f9379d3454dc898e9dc8223bb50c745690c
ylluminarious <georgedp@orbitalimpact.com>
2015-06-11 19:05:11 -0400

    fixed lagging issues with spacebar and clicking / touching

--- app/states/playing.rb
+++ app/states/playing.rb
@@ -1,11 +1,11 @@
 class Playing < MasterState
   def initialize
-    @game_over_counter = 0
-    @game_over = false
+    @game_over    = false
+    @down_counter = 0
   end
   
   def create
-    jump = proc do
+    @jump_or_try_again = proc do
       if @game_over == false
         @@flutterjump.jumping = true
         @@flutterjump.fluttershy.sprite.body.velocity.y = Constants::JUMP_VELOCITY
@@ -13,6 +13,18 @@ class Playing < MasterState
         @@flutterjump.fluttershy.sprite.load_texture(@@flutterjump.fluttershy.flying_key)
         @@flutterjump.fluttershy.sprite.body.set_size(Constants::FLYING_WIDTH, Constants::FLYING_HEIGHT)
         @@flutterjump.fluttershy.sprite.animations.play(@@flutterjump.fluttershy.flying_key)
+      else
+        @@flutterjump.background.sprite.destroy
+        @@flutterjump.obstacles.group.destroy
+        @@flutterjump.ground.sprite.destroy
+        @@flutterjump.fluttershy.sprite.destroy
+        
+        @game_over_text.destroy
+        @try_again_text.destroy
+        
+        @@flutterjump.score.amount = 0
+        @game_over = false
+        create
       end
     end
     
@@ -22,7 +34,11 @@ class Playing < MasterState
       object.create
     end
     
-    @@flutterjump.keys.spacebar.on(:down, &jump)
+    if @down_counter == 0
+      @@flutterjump.keys.spacebar.on(:down, &@jump_or_try_again)
+      @@phaser_game.input.on(        :down, &@jump_or_try_again)
+      @down_counter += 1
+    end
   end
   
   def update
@@ -47,10 +63,9 @@ class Playing < MasterState
         @@flutterjump.fluttershy.sprite.body.set_size(Constants::OUCH_WIDTH, Constants::OUCH_HEIGHT)
         @@flutterjump.fluttershy.stop_moving
         
-        keys_to_remove = [Phaser::Keyboard::LEFT, Phaser::Keyboard::RIGHT, Phaser::Keyboard::A, Phaser::Keyboard::D]
-        keys_to_remove.each do |key|
-          @@phaser_game.input.keyboard.remove_key(key)
-        end
+        @@flutterjump.keys.spacebar.reset(:hard)
+        
+        @@flutterjump.keys.spacebar.on(:down, &@jump_or_try_again)
       end
     end
     
@@ -83,28 +98,5 @@ class Playing < MasterState
       @@flutterjump.fluttershy.sprite.body.set_size(Constants::WALKING_WIDTH, Constants::WALKING_HEIGHT)
       @@flutterjump.fluttershy.sprite.animations.play(@@flutterjump.fluttershy.walking_key)
     end
-    
-    try_again = proc do
-      if @game_over_counter == 0 && @game_over == true
-        @@flutterjump.background.sprite.destroy
-        @@flutterjump.obstacles.group.destroy
-        @@flutterjump.ground.sprite.destroy
-        @@flutterjump.fluttershy.sprite.destroy
-        
-        @game_over_text.destroy
-        @try_again_text.destroy
-        
-        @@flutterjump.score.amount = 0
-        @game_over = false
-        create
-          
-        @game_over_counter += 1
-      elsif @game_over == false
-        @game_over_counter = 0
-      end
-    end
-    
-    @@phaser_game.input.on(:down, &try_again)
-    @@flutterjump.keys.spacebar.on(:down, &try_again)
   end
 end
