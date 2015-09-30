7943dbd44b260fb01142c6e5933c85111cec7d87
George D. Plymale II <georgedp@orbitalimpact.com>
2015-09-30 09:53:40 -0400

    use m4a codec on iOS devices

--- app/game_objects/sounds.rb
+++ app/game_objects/sounds.rb
@@ -6,24 +6,35 @@ class Sounds < GameObject
   attr_reader :audio_objects
   
   def initialize
-    @jump_key       = "jump"
-    @jump_path      = ["assets/audio/jump.ogg",       "assets/audio/jump.mp3",       "assets/audio/jump.m4a"]
+    @jump_key               = "jump"
+    @non_ios_jump_path      = ["assets/audio/jump.ogg",       "assets/audio/jump.mp3"]
+    @ios_jump_path          = "assets/audio/jump.m4a"
     
-    @score_key      = "score"
-    @score_path     = ["assets/audio/score.ogg",      "assets/audio/score.mp3",      "assets/audio/score.m4a"]
+    @score_key              = "score"
+    @non_ios_score_path     = ["assets/audio/score.ogg",      "assets/audio/score.mp3"]
+    @ios_score_path         = "assets/audio/score.m4a"
     
-    @music_key      = "music"
-    @music_path     = ["assets/audio/game_music.ogg", "assets/audio/game_music.mp3", "assets/audio/game_music.m4a"]
+    @music_key              = "music"
+    @non_ios_music_path     = ["assets/audio/game_music.ogg", "assets/audio/game_music.mp3"]
+    @ios_music_path         = "assets/audio/game_music.m4a"
     
-    @game_over_key  = "game over"
-    @game_over_path = ["assets/audio/game_over.ogg",  "assets/audio/game_over.mp3",  "assets/audio/game_over.m4a"]
+    @game_over_key          = "game over"
+    @non_ios_game_over_path = ["assets/audio/game_over.ogg",  "assets/audio/game_over.mp3"]
+    @ios_game_over_path     = "assets/audio/game_over.m4a"
   end
   
   def preload
-    @@game.load.audio(@jump_key,      @jump_path)
-    @@game.load.audio(@score_key,     @score_path)
-    @@game.load.audio(@music_key,     @music_path)
-    @@game.load.audio(@game_over_key, @game_over_path)
+    if @@game.device.iOS
+      @@game.load.audio(@jump_key,      @ios_jump_path)
+      @@game.load.audio(@score_key,     @ios_score_path)
+      @@game.load.audio(@music_key,     @ios_music_path)
+      @@game.load.audio(@game_over_key, @ios_game_over_path)
+    else
+      @@game.load.audio(@jump_key,      @non_ios_jump_path)
+      @@game.load.audio(@score_key,     @non_ios_score_path)
+      @@game.load.audio(@music_key,     @non_ios_music_path)
+      @@game.load.audio(@game_over_key, @non_ios_game_over_path)
+    end
   end
   
   def create
