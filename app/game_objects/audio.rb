71fbbb6b2fa1c5efd39b5e595d5124b2b12e5eb8
George D. Plymale II <georgedp@orbitalimpact.com>
2015-10-01 16:56:48 -0400

    move sounds into their own subdirectories and fix a typo

--- app/game_objects/audio.rb
+++ app/game_objects/audio.rb
@@ -8,29 +8,29 @@ class Audio < GameObject
   attr_reader :sounds
   
   def initialize
-    @jump_key                 = "jump"
-    @non_ios_jump_path        = ["assets/audio/jump.ogg",        "assets/audio/jump.mp3"]
-    @ios_jump_path            = "assets/audio/jump.m4a"
-    
-    @score_key                = "score"
-    @non_ios_score_path       = ["assets/audio/score.ogg",       "assets/audio/score.mp3"]
-    @ios_score_path           = "assets/audio/score.m4a"
-    
     @title_music_key          = "title music"
-    @non_ios_title_music_path = ["assets/audio/title_music.ogg", "assets/audio/title_music.mp3"]
-    @ios_title_music_path     = "title music"
+    @non_ios_title_music_path = ["assets/audio/music/title_music.ogg", "assets/audio/music/title_music.mp3"]
+    @ios_title_music_path     = "assets/audio/music/title_music.m4a"
     
     @story_music_key          = "story music"
-    @non_ios_story_music_path = ["assets/audio/story_music.ogg", "assets/audio/story_music.mp3"]
-    @ios_story_music_path     = "assets/audio/story_music.m4a"
+    @non_ios_story_music_path = ["assets/audio/music/story_music.ogg", "assets/audio/music/story_music.mp3"]
+    @ios_story_music_path     = "assets/audio/music/story_music.m4a"
     
     @game_music_key           = "game music"
-    @non_ios_game_music_path  = ["assets/audio/game_music.ogg",  "assets/audio/game_music.mp3"]
-    @ios_game_music_path      = "assets/audio/game_music.m4a"
+    @non_ios_game_music_path  = ["assets/audio/music/game_music.ogg",  "assets/audio/music/game_music.mp3"]
+    @ios_game_music_path      = "assets/audio/music/game_music.m4a"
+    
+    @jump_key                 = "jump"
+    @non_ios_jump_path        = ["assets/audio/effects/jump.ogg",        "assets/audio/effects/jump.mp3"]
+    @ios_jump_path            = "assets/audio/effects/jump.m4a"
+    
+    @score_key                = "score"
+    @non_ios_score_path       = ["assets/audio/effects/score.ogg",       "assets/audio/effects/score.mp3"]
+    @ios_score_path           = "assets/audio/effects/score.m4a"
     
     @game_over_key            = "game over"
-    @non_ios_game_over_path   = ["assets/audio/game_over.ogg",   "assets/audio/game_over.mp3"]
-    @ios_game_over_path       = "assets/audio/game_over.m4a"
+    @non_ios_game_over_path   = ["assets/audio/effects/game_over.ogg",   "assets/audio/effects/game_over.mp3"]
+    @ios_game_over_path       = "assets/audio/effects/game_over.m4a"
   end
   
   def preload
